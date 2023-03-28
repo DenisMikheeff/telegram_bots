2c87ad7a1efcb18c0a1ac70e4ca40258dd93384a
DenisMikheeff <98670348+DenisMikheeff@users.noreply.github.com>
2023-03-28 18:18:45 +0300

    Update cyprus_tax_bot.py

--- cyprus_tax_bot.py
+++ cyprus_tax_bot.py
@@ -5,12 +5,12 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Conve
 # Define the start function
 def start(update, context):
     update.message.reply_text("Please enter an integer")
+    s = int(update.message.text)
     return "integer"
 
 # Define the integer handler function
 def handle_integer(update, context):
     try:
-        s = int(update.message.text)
         if s >= 1:
             return "q1"
         else:
