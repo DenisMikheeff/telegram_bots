e12b16055656add3d4267225cf6e706ff119fa53
DenisMikheeff <98670348+DenisMikheeff@users.noreply.github.com>
2023-03-28 19:10:41 +0300

    Update cyprus_tax_bot.py

--- cyprus_tax_bot.py
+++ cyprus_tax_bot.py
@@ -1,16 +1,22 @@
 import telegram
 import os
+import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 
-# Define the start function
-def start(update, context):
-    update.message.reply_text("Please enter an integer")
+# Define a function to handle incoming messages
+def handle_message(update, context):
+    text = update.message.text
     try:
-        int_s = int(update.message.text.split()[-1])
+        int_s = int(text)
         context.user_data['s'] = int_s
         update.message.reply_text("Got it, your integer input is {}.".format(int_s))
     except ValueError:
         update.message.reply_text("Oops, please enter a valid integer.")
+
+# Define a function to start the bot
+def start(update, context):
+    update.message.reply_text("Please enter an integer")
 
 # Define the integer handler function
 def handle_integer(update, context):
@@ -82,18 +88,20 @@ def main():
     PORT = int(os.environ.get("PORT", 8443))
     APP_NAME = "cyprus-tax-bot"
 
-    updater = Updater(TOKEN, use_context=True)
+    updater = Updater(token='5486890272:AAEPDRADV5rVOLk7vRvRouXIi7Hkcc5V8RE', use_context=True)
+    dispatcher = updater.dispatcher
 
     dp = updater.dispatcher
 
-    dp.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
 
     conv_handler = MessageHandler(Filters.text & ~Filters.command, handle_integer)
     dp.add_handler(conv_handler)
 
     dp.add_handler(MessageHandler(Filters.regex("(?i)yes|no"), handle_q1))
     dp.add_handler(MessageHandler(Filters.regex("(?i)yes|no"), handle_q2))
-
+    
     updater.start_webhook(
         listen="0.0.0.0",
         port=PORT,
@@ -101,6 +109,8 @@ def main():
         webhook_url=f"https://cyprus-tax-bot.herokuapp.com/5486890272:AAEPDRADV5rVOLk7vRvRouXIi7Hkcc5V8RE"
     )
 
+    updater.start_polling()
+    logging.info('Bot started')
     updater.idle()
 
 if __name__ == '__main__':
